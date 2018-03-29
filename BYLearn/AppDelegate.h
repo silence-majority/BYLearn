a6cbcbe43e6abf5f443c71beee97e2a894e78961
xxp <931968649@qq.com>
2018-03-29 15:05:46 +0800

    Initial Commit

---
+++ BYLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BYLearn
+//
+//  Created by xy_yanfa_imac on 2018/3/29.
+//  Copyright © 2018年 xy_yanfa_imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
